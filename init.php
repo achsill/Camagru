0ccca8d6091916d7977357941941e4d2450bbade
hlouar <hamzalouar@gmail.com>
2018-02-15 12:26:13 +0100

    init.php OK

--- init.php
+++ init.php
@@ -1,18 +1,22 @@
 <?php
-include "config.php";
+$servername = "mysql:host=localhost:3306";
+$username = "root";
+$password = "root";
 
-$initDB = $db->query("CREATE TABLE account(
+// Verifier si la db n'existe pas, si oui pas besoin de la créer !!!!
+try {
+  $dbh = new PDO($servername, $username, $password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo 'Failed';
+}
+$dbh->exec("CREATE DATABASE camagru;");
+$dbh->exec("USE camagru;");
+$dbh->exec("CREATE TABLE account(
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
   );
   ");
-
-
-if ($db->query($initDB) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $db->error;
-}
 ?>
