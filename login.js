b1bcf3cfc7b474441906b77dd9684cdb8aa9d4dd
hlouar <hamzalouar@gmail.com>
2018-02-17 09:56:46 +0100

    merge

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ function createRequestObject()
     return http;
 }
 
-function gestionClic()
+function SignUpForm()
 {
     http = createRequestObject();
     var post = "email=" + document.getElementById('email').value
@@ -23,7 +23,7 @@ function gestionClic()
     post = post + "&confirmPassword=" + document.getElementById('confirmPassword').value
 
     http.onreadystatechange = handleAJAXReturn;
-    http.open("POST", "./form.php", true);
+    http.open("POST", "./signUpForm.php", true);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     http.send(post);
 }
@@ -43,4 +43,29 @@ function handleAJAXReturn()
             alert('Pas glop pas glop');
         }
     }
+}
+
+
+function SigninForm()
+{
+    http = createRequestObject();
+    var post = "email=" + document.getElementById('loginEmail').value
+    post = post + "&password=" + document.getElementById('loginPassword').value
+
+    http.onreadystatechange = handleAJAXReturn;
+    http.open("POST", "./signUpForm.php", true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.send(post);
+}
+
+
+function ToggleCard() {
+  if (document.getElementById("signUpForm").style.display == 'none') {
+    document.getElementById("signUpForm").style.display = 'block';
+    document.getElementById("signInForm").style.display = 'none';
+  }
+  else {
+    document.getElementById("signUpForm").style.display = 'none';
+    document.getElementById("signInForm").style.display = 'block';
+  }
 }
---
+++ signInForm.php
@@ -0,0 +1,24 @@
+<?php
+// include "config.php";
+
+class User {
+	private $db;
+
+	function __construct($db) {
+		$this->db = $db;
+  }
+}
+
+$servername = "mysql:dbname=camagru;host=localhost:3306";
+$username = "root";
+$password = "root";
+
+try {
+	$db = new PDO($servername, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (PDOException $e) {
+	echo 'Connexion échouée : ' . $e->getMessage();
+}
+// TO CHANGE
+
+$newUser = new User($db);
+echo $newUser->CreateUser($_POST['username'],$_POST['password'], $_POST['confirmPassword'], $_POST['email']);
