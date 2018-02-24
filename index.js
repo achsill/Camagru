bad9f79a50ea38f2ce93d50aeb41f708684b710a
hlouar <hamzalouar@gmail.com>
2018-02-24 17:10:15 +0100

    infinityScroll

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ getInfo();
 userLogged = -1;
 username = '';
 usersPics = [];
+nbrPictures = 10;
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+    nbrPictures+= 10;
+    getInfo();
+    console.log("Bottom of page");
+  }
+};
 
 function getInfo() {
   http.onreadystatechange = isUserLogged;
@@ -19,7 +28,7 @@ function printPictures() {
   var content = '';
   i = usersPics.length - 1;
   length = usersPics.length - 1;
-  while (i > length - 10) {
+  while (i > length - nbrPictures) {
     if (usersPics[i]) {
       content = content +'<div class="userPost">' + '<img  src="user_pictures/' + usersPics[i] + '" alt="">' + '<div class="interact"> <div class="containLikeBtn"> <img class="likeBtn" src="img/like.png" alt=""> </div> <div class="containComment"> <p>Show the comments</p> </div> </div> </div>';
     }
